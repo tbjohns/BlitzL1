81b7ef7c30e115ba559ffcedd9c5e37033f4c098
mathurinm <mathurin.massias@gmail.com>
2017-08-16 10:53:16 +0200

    FIX: relative imports for python3

--- python/__init__.py
+++ python/__init__.py
@@ -1,15 +1,13 @@
-from _blitzl1 import LassoProblem
-from _blitzl1 import LogRegProblem
+from ._blitzl1 import LassoProblem
+from ._blitzl1 import LogRegProblem
 
-from _blitzl1 import load_solution
+from ._blitzl1 import load_solution
 
-from _blitzl1 import set_tolerance
-from _blitzl1 import get_tolerance
-from _blitzl1 import set_max_time
-from _blitzl1 import get_max_time
-from _blitzl1 import set_use_intercept
-from _blitzl1 import get_use_intercept
-from _blitzl1 import set_verbose
-from _blitzl1 import get_verbose
-
-
+from ._blitzl1 import set_tolerance
+from ._blitzl1 import get_tolerance
+from ._blitzl1 import set_max_time
+from ._blitzl1 import get_max_time
+from ._blitzl1 import set_use_intercept
+from ._blitzl1 import get_use_intercept
+from ._blitzl1 import set_verbose
+from ._blitzl1 import get_verbose
